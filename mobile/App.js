9c46d981af1e9c10fd508ea51e65ffbb743d4f96
Ajay Bhattacharyya <ajay.bhattacharyya.16@gmail.com>
2024-08-10 21:25:55 -0400

    removed old mobile app

--- mobile/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { WebView } from 'react-native-webview'
-
-export default function App(){
-  return (
-    <WebView source={{ uri: 'https://mikeoxlongbanking.firebaseapp.com/#'}} />
-  )
-}
