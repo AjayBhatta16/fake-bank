ddbe36c147a9caaf072e5f3dbb111238c63533b9
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2025-03-01 02:35:35 -0500

    Transfers functioning properly again

--- client/src/components/exchange/AccountDropdown.js
+++ client/src/components/exchange/AccountDropdown.js
@@ -1,11 +1,19 @@
 import React from "react"
 import MaybeDisplay from "../common/MaybeDisplay"
+import { useSelector } from "react-redux"
 
 export default function AccountDropdown(props) {
+    const selectedAccount = useSelector(state => state.accounts.selectedAccount)
+
     return (
         <div className="form-group">
             <label for={props.formName} className="text-info">{props.displayName}:</label>
-            <select className="form-control" id="from" onChange={props.changeAction}>
+            <select 
+                className="form-control" 
+                id="from" 
+                onChange={props.changeAction}
+                defaultValue={selectedAccount.accountNumber.toString()}
+            >
                 { props.accounts.map(acc => (
                     <option
                         value={acc.accountNumber.toString()}
